7db5b25f14e58c9fe88d51e893978bd88087e042
José <josepedrocandido@gmail.com>
2020-03-25 02:16:18 +0000

    Home page and buttons section

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -1,1 +1,33 @@
 window.sr=ScrollReveal();
+
+
+
+
+
+var $green = "#33CC99";
+var $blue = "#3399CC";
+var $orange = "#FF9933";
+
+function changeColor(color) {
+  var x = document.getElementsByClassName("border-color-changer");
+  var y = document.getElementsByClassName("background-color-changer");
+  var i;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.borderColor = color;
+    x[i].style.stroke = color;
+  }
+  for (i = 0; i < y.length; i++) {
+    y[i].style.backgroundColor = color;
+  }
+}
+
+var header = document.getElementById("buttons");
+var btns = header.getElementsByClassName("button-square");
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
